94a569defaa43f6a8c5ac992cae27d47b3fbed04
BDogs <1215852067@qq.com>
2016-06-06 13:19:50 +0800

    Initial Commit

---
+++ SlidingMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlidingMenu
+//
+//  Created by 诸葛游 on 16/6/6.
+//  Copyright © 2016年 fengYeHen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SlidingMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlidingMenu
+//
+//  Created by 诸葛游 on 16/6/6.
+//  Copyright © 2016年 fengYeHen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
